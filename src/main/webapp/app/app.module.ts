8277844335b4e43e63eb3e1a60d6dd3943ae16ca
RuwanKarunanayake <ruwan.14@cse.mrt.ac.lk>
2017-05-02 08:09:18 +0530

    initial commit

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,55 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { PmsSharedModule, UserRouteAccessService } from './shared';
+import { PmsHomeModule } from './home/home.module';
+import { PmsAdminModule } from './admin/admin.module';
+import { PmsAccountModule } from './account/account.module';
+import { PmsEntityModule } from './entities/entity.module';
+
+import { LayoutRoutingModule } from './layouts';
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ErrorComponent
+} from './layouts';
+
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        PmsSharedModule,
+        PmsHomeModule,
+        PmsAdminModule,
+        PmsAccountModule,
+        PmsEntityModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        { provide: Window, useValue: window },
+        { provide: Document, useValue: document },
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class PmsAppModule {}
